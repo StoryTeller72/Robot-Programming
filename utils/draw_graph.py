2767185af4f057eebcace0c77198db32df8769dd
Rustam Antinov <rustamantinov@gmail.com>
2025-05-06 11:21:30 +0300

    Draw plot with results

--- utils/draw_graph.py
+++ utils/draw_graph.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 value_base = np.load(
-    '/home/rustam/PR/base_DDPG_2.npy')
+    'sucsess_rates/base_DDPG_2.npy')
 values_her = np.load(
-    '/home/rustam/PR/DDPG_HER_not_terminated_0.02.npy')
+    'sucsess_rates/DDPG_HER_not_terminated_0.02.npy')
 values_her_5 = np.load(
-    '/home/rustam/PR/DDPG_HER_not_terminated.npy')
+    'sucsess_rates/DDPG_HER_not_terminated.npy')
 index = np.array([i for i in range(30)])
 plt.axis([0, 29, 0, 1])
 plt.title("Results")
